54fc2e5dc760a534311d0333e1903589a8e9f49a
Diksha Jakhmola <66503818+diksha-j17@users.noreply.github.com>
2022-06-07 20:40:34 +0530

    Add files via upload

---
+++ lab6ques5.js
@@ -0,0 +1,4 @@
+var msg = "Hello World!!";
+console.log("Variable before changing value :: " + msg);
+msg = "Hello World!! Have a nice day !!";
+console.log("Variable after changing value :: " + msg);
